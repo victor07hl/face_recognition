014b3458d524ad4b781e70da0427f14161e32f7c
victormoreno <victor07hl@gmail.com>
2021-05-22 11:52:18 -0500

    only encoding imgs

--- webcamp.py
+++ webcamp.py
@@ -11,6 +11,20 @@ list_img= os.listdir(path_imgs)
 print(list_img)
 print('entering in for')
 for img_name in list_img:
-	#images.append(cv2.imread(f'{path_imgs}/{img_name}'))
-	#names_imgs.append(os.path.splitext(img_name[0]))
+	imgadd = cv2.imread(f'{path_imgs}/{img_name}')
+	imgadd = cv2.cvtColor(imgadd, cv2.COLOR_BGR2RGB)
+	images.append(imgadd)
+	names_imgs.append(os.path.splitext(img_name)[0])
 	print(os.path.splitext(img_name)[0])
+
+#Creating encoding functions
+def findEncodings(images):
+	encode_list = []
+	for img in images:
+		encode = face_recognition.face_encodings(img)[0]
+		encode_list.append(encode)
+	return encode_list
+
+
+encodelistknow = findEncodings(images)
+print(f'encoding {len(encodelistknow)} images')
