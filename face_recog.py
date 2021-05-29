13ac4b837f76c6f7dab7650dfb413ef63a1476a6
victormoreno <victor07hl@gmail.com>
2021-05-29 10:24:51 -0500

    change path of imgs because folder imgs was moved to main_user folder

--- face_recog.py
+++ face_recog.py
@@ -2,23 +2,24 @@ import cv2
 import numpy as np
 import face_recognition
 
-imgElon = face_recognition.load_image_file('imgs/elon_musk.jpg')
+imgElon = face_recognition.load_image_file('main_user/imgs/train/elon_musk.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 
-imgtest = face_recognition.load_image_file('imgs/gates.jpg')
+imgtest = face_recognition.load_image_file('main_user/imgs/train/gates.jpg')
 imgtest = cv2.cvtColor(imgtest,cv2.COLOR_BGR2RGB)
 
 
 #face locations 
 faceloc = face_recognition.face_locations(imgElon)[0]
+print(faceloc)
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 
 faceloctest = face_recognition.face_locations(imgtest)[0]
 encodetest = face_recognition.face_encodings(imgtest)[0]
 
 #drawing rectangles
-cv2.rectangle(imgElon,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),2)
-cv2.rectangle(imgtest,(faceloctest[3],faceloctest[0]),(faceloctest[1],faceloctest[2]),(255,0,255),2)
+cv2.rectangle(imgElon,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),1)
+cv2.rectangle(imgtest,(faceloctest[3],faceloctest[0]),(faceloctest[1],faceloctest[2]),(255,0,255),1)
 
 #Results comparison
 results = face_recognition.compare_faces([encodeElon],encodetest)
